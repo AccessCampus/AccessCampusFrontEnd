ec2283e07bf8b4044ae396c900db026e9cb5a29e
Eric Kim <erickim195@gmail.com>
2020-03-26 21:30:29 -0700

    distillery configuration

--- access_ubc/config/prod.exs
+++ access_ubc/config/prod.exs
@@ -16,15 +16,17 @@ config :access_ubc, AccessUbcWeb.Endpoint,
   secret_key: "${SECRET_KEY_BASE}"
 
 config :access_ubc, AccessUbcWeb.Endpoint,
-  # Possibly not needed, but doesn't hurt
-  http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  server: true
+  load_from_system_env: true,
+  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  server: true, # Without this line, your app will not start the web server!
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :access_ubc, AccessUbc.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
+  url: "${DATABASE_URL}",
+  database: "",
   ssl: true,
   pool_size: 2
 
