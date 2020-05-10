4a1556816ca44c10de0e6e87b2eaa439db54a0fb
Eric Kim <erickim195@gmail.com>
2020-05-10 09:41:12 -0700

    example multiple selector

--- src/components/building-selector/BuildingSelector.js
+++ src/components/building-selector/BuildingSelector.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import capitalizeCampusName from '../../utils/capitalizeCampusName'
+import React, { useState } from 'react';
+import Select from '@material-ui/core/Select';
+import capitalizeCampusName from '../../utils/capitalizeCampusName';
 
 const BuildingSelector = ({ campus }) => {
     const campusText = capitalizeCampusName(campus);
+    const [personName, setPersonName] = useState([]);
+
+    const names = [
+        'Oliver Hansen',
+        'Van Henry',
+        'April Tucker',
+        'Ralph Hubbard',
+        'Ralph Hubbard1',
+        'Ralph Hubbard2',
+        'Ralph Hubbard3',
+        'Ralph Hubbard4',
+        'Ralph Hubbard5',
+    ];
+
+    const handleChangeMultiple = ({ target: options }) => {
+        const value = [];
+        for (option of options) {
+            if (option.selected) value.push(option.value);
+        }
+        setPersonName(value);
+    }
 
     return (
         <div className="campus-selector">
-            Choose your building on the {campusText} Campus!
+            <Select
+                multiple
+                native
+                value={personName}
+                onChange={handleChangeMultiple}
+            >
+                {names.map(name => (
+                    <option key={name} value={name}>
+                        {name}
+                    </option>
+                ))}
+            </Select>
         </div>
     );
 }
