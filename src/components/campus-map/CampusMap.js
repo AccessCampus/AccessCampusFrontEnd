06f682f346062234db9184e846acd4a6025f749e
Eric Kim <erickim195@gmail.com>
2020-05-09 21:50:35 -0700

    render simple map to CampusMap

--- src/components/campus-map/CampusMap.js
+++ src/components/campus-map/CampusMap.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 const CampusMap = ({ campus, color }) => {
+    const mapStyles = {
+        height: "100vh",
+        width: "100%",
+    }
+
+    const defaultCenter = {
+        lat: 41.3851, lng: 2.1734
+    }
+
     return (
         <div className="campus-map">
-            {campus} + {color}
+            <LoadScript
+                googleMapsApiKey="AIzaSyCDgTipX4nMXDFHiSkw0fSo0idfZsOecAs">
+                <GoogleMap
+                    mapContainerStyle={mapStyles}
+                    zoom={13}
+                    center={defaultCenter}
+                />
+            </LoadScript>
         </div>
     );
 };
