74d0b2faacc1f99af00c5553a9fbd00ed014604b
Eric Kim <erickim195@gmail.com>
2020-05-10 09:12:36 -0700

    refactor CampusSelector -> BuildingSelector

---
+++ src/components/building-selector/BuildingSelector.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BuildingSelector = ({ campus }) => {
+    return (
+        <div className="campus-selector">
+            Choose your building on the {campus} Campus!
+        </div>
+    );
+}
+
+export default BuildingSelector;
--- src/components/campus-page/CampusPage.js
+++ src/components/campus-page/CampusPage.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import CampusMap from '../campus-map/CampusMap';
+import BuildingSelector from '../building-selector/BuildingSelector';
 
 const CampusPage = ({ campus, color, index }) => {
     return (
         <div className="campus-page">
-            <CampusMap
-                key={index}
-                index={index}
-                campus={campus}
-                color={color}
-            />
+            <Grid container>
+                <Grid item xs={12} md={6} item>
+                    <CampusMap
+                        key={index}
+                        index={index}
+                        campus={campus}
+                        color={color}
+                    />
+                </Grid>
+                <Grid item xs={12} md={6} item>
+                    <BuildingSelector
+                        campus={campus}
+                    />
+                </Grid>
+            </Grid>
         </div>
     );
 }
