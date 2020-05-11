61e499bd813b09f193ddca73913b84ffb0c76419
Eric Kim <erickim195@gmail.com>
2020-05-10 20:30:26 -0700

    moved async request to access-campus-api to BuildingSelector Component to ensure correct component renders upon request of data

--- src/components/building-selector/BuildingSelector.js
+++ src/components/building-selector/BuildingSelector.js
@@ -1,20 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Select from '@material-ui/core/Select';
 import capitalizeCampusName from '../../utils/capitalizeCampusName';
 
-const BuildingSelector = ({ campus, buildings }) => {
+const BuildingSelector = ({ campus }) => {
     const campusText = capitalizeCampusName(campus);
-    const [campusBuilding, setCampusBuildingNames] = useState([]);
+    const [buildings, setBuildings] = useState([]);
+
+    useEffect(() => {
+        async function getBuildings() {
+            const res = await axios.get("https://access-campus-api.herokuapp.com/api/buildings");
+            setBuildings(res.data.data);
+        }
+        getBuildings();
+    }, []);
 
     return (
         <div className="campus-selector">
             <Select
                 multiple
                 native
-                value={campusBuilding}
+                value={buildings}
             >
-                <option>
-                    Hello
+                <option
+                    key={
+                        buildings.length === 0 ?
+                            "none" : buildings[0].id
+                    }
+                    value={
+                        buildings.length === 0 ?
+                            "none" : buildings[0].id
+                    }
+                >
+                    {
+                        buildings.length === 0 ?
+                            "none" : buildings[0].id
+                    }
                 </option>
                 ))}
             </Select>
